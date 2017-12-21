63b15a95a75ccf68e902d362c5a4331be402c89e
xo-wendyx <wendylovespandas89@gmail.com>
2017-12-21 00:21:52 -0800

    Create badgalriri.js

---
+++ badgalriri.js
@@ -0,0 +1,39 @@
+function bad(elem){
+  elem.classList.toggle("riri");
+}
+
+function gal(elem){
+  elem.classList.toggle("fenty");
+}
+
+function queen(elem){
+  elem.classList.toggle("fashion");
+}
+
+function beauty(elem){
+  elem.classList.toggle("major");
+}
+
+function anti(elem){
+  elem.classList.toggle("journal");
+}
+
+function damn(elem){
+  elem.classList.toggle("dior");
+}
+
+function king(elem){
+  elem.classList.toggle("vogue");
+}
+
+function badgalriri(elem){
+    elem.classList.toggle("done")
+}
+
+function playRiri(elem) {
+  document.getElementsById("consideration").play();
+}
+
+function pauseRiri(elem){
+  document.getElementById("consideration").pause();
+}
